367ef083065b1c5685bd1f6b8da636b542604417
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2024-10-20 16:08:53 +0300

    added SparseTable.java

---
+++ Classes/SparseTable.java
@@ -0,0 +1,43 @@
+class SparseTable{
+    int[][] table;
+    int log;
+    int[] floorPow;
+    static int zero = 0;
+    private int operation(int a, int b){
+        // define associative operation here (op(op(a,b).c)=op(a,op(b,c)
+    }
+    public SparseTable(int[] a){
+        log=2; int n=a.length;
+        while(n>0){
+            log++;
+            n>>=1;
+        }
+        table = new int[a.length][log];
+        for(int i=0; i<a.length; ++i) table[i][0]=a[i];
+        for(int j=1; j<log; ++j){
+            for(int i=0; i+(1<<j-1)<a.length; ++i){
+                table[i][j]=operation(table[i][j-1],table[i+(1<<(j-1))][j-1]);
+            }
+        }
+        floorPow= new int[a.length+1];
+        floorPow[0]=-1;
+        for(int i=1; i<a.length; ++i){
+            floorPow[i]=floorPow[i-1];
+            if((i&(i-1))==0) floorPow[i]++;
+        }
+    }
+    public int get(int l, int r){
+        int x = floorPow[r-l+1];
+        // if operation is idempotent (op(a,a)=a) (like min, max, gcd, lcm...)
+        return operation(table[l][x],table[r-(1<<x)+1][x]); // O(f(x))
+        // if operation is not idempotent
+        int ans = table[l][x];
+        l+=1<<x;
+        while(l<=r){
+            x = floorPow[r-l+1];
+            ans = operation(ans, table[l][x]);
+            l+=1<<x;
+        }
+        return ans; // O(ln(r-l+1).f(x))
+    }
+}
