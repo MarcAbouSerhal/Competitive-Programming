637cdea1d1e73dcc7e20970cad5f0c0292c75199
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2024-12-01 13:07:02 +0200

    added LinearSieve.java

---
+++ Classes/Math/LinearSieve.java
@@ -0,0 +1,50 @@
+class LinearSieve{
+    static class Pair{
+        int x,y;
+        public Pair(int x, int y){ this.x=x; this.y=y; }
+    }
+    int[] sp;
+    int[] pf;
+    public LinearSieve(int c){
+        sp = new int[c+1];
+        pf = new int[c+1];
+        for(int i=2; i<=c; ++i)
+            if(sp[i] == 0)
+                for(int j=1; j<=c/i; ++j)
+                    if(sp[i*j] == 0) 
+                        sp[i*j] = i;
+        for(int i=2; i<=c; ++i){
+            pf[i] = pf[i/sp[i]];
+            if(sp[i/sp[i]] != sp[i]) ++pf[i];
+        }
+    }
+    public ArrayList<Pair> primeFactors(int x){
+        ArrayList<Pair> res = new ArrayList<>();
+        while(x>1){
+            int p = sp[x], cnt = 0;
+            while(sp[x] == p){
+                x /= sp[x];
+                cnt++;
+            }
+            res.add(new Pair(p, cnt));
+        }
+        return res;
+    }
+    public ArrayList<Integer> divisors(int x){
+        ArrayList<Pair> pfs = primeFactors(x);
+        ArrayList<Integer> ans = new ArrayList<>();
+        f(1,0,pfs,ans);
+        ans.add(1);
+        Collections.sort(ans);
+        return ans;
+    }
+    private static void f(int x, int i, ArrayList<Pair> pfs,ArrayList<Integer> ans){
+        if(i>=pfs.size()) return;
+        f(x,i+1,pfs,ans);
+        for(int cnt=1; cnt<=pfs.get(i).y; ++cnt){
+            x *= pfs.get(i).x;
+            ans.add(x);
+            f(x,i+1,pfs,ans);
+        }
+    }
+}
