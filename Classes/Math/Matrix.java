bc9ec8c5842c27883f335240e46d2f2db08227ee
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2024-12-09 01:29:15 +0200

    Recurrence.java is now Matrix.java, now calculates full matrix instead of just finding top row and evaluating directly

---
+++ Classes/Math/Matrix.java
@@ -0,0 +1,61 @@
+class Matrix{
+    public static long[][] mul(long[][] a, long[][] b, long mod){
+        long[][] c = new long[a.length][b[0].length];
+        for(int i=0; i<a.length; ++i)
+            for(int k=0; k<b.length; ++k)
+                for(int j=0; j<b[0].length; ++j)
+                    c[i][j] = (c[i][j] + a[i][k]*b[k][j])%mod;
+        for(int i=0; i<a.length; ++i)
+            for(int j=0; j<b[0].length; ++j)
+                if(c[i][j]<0) c[i][j] += mod;
+        return c;
+    }
+    public static long[][] TpowN(long[] c, long n, long mod){
+        // returns T^n in k^2.log(n) time iff T is of the form
+        // (  0   1   0   0   ...   0   0  )
+        // (  0   0   1   0   ...   0   0  )
+        // (  0   0   0   1   ...   0   0  )
+        // (  .   .   .   .   ...   .   .  )
+        // (  0   0   0   0   ...   1   0  )
+        // (  0   0   0   0   ...   0   1  )
+        // ( ck  ck-1 .....   ...   c2  c1 )
+        int k = c.length;
+        long[][] ans = new long[k][]; 
+        long[] g = new long[k+1];
+        g[k] = 1;
+        for(int i=1; i<=k; ++i) g[k-i] = (-c[i-1]+mod)%mod;
+        ans[0] = xPowNModG(n, g, k, mod);
+        for(int i=1; i<k; ++i){
+            ans[i] = new long[k];
+            long m = ans[i-1][k-1];
+            for(int j=1; j<k; ++j) 
+                ans[i][j] = (ans[i-1][j-1]-m*g[j]+mod)%mod;
+            ans[i][0] = (-m*g[0]+mod)%mod;
+        }
+        return ans;
+    }
+    public static long[] xPowNModG(long n, long[] g, int k, long mod){
+        if(n<k){
+            long[] res = new long[k];
+            res[(int)n] = 1;
+            return res;
+        }
+        long[] half = xPowNModG(n>>1, g, k, mod);
+        long[] last = new long[k<<1];
+        for(int i=0; i<k; ++i)
+            for(int j=0; j<k; ++j)
+                last[i+j] = (last[i+j] + half[i]*half[j])%mod;
+        if(n%2==1){
+            for(int i=(k<<1)-1; i>0; --i) last[i] = last[i-1];
+            last[0] = 0;
+        }
+        for(int i=(k<<1)-1; i>=k; --i)  
+            if(last[i]!=0){
+                long m = last[i];
+                for(int j=0; j<=k; ++j)
+                    last[i-j] = (last[i-j] - m*g[k-j])%mod;
+            }
+        for(int i=0; i<k; ++i) half[i] = (last[i]+mod)%mod;
+        return half;
+    }
+}
