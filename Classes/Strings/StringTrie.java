8a5fc5b489f0956329f32317973ba832c18c2156
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2024-12-16 01:00:04 +0200

    cleaned up StringTrie

--- Classes/Strings/StringTrie.java
+++ Classes/Strings/StringTrie.java
@@ -16,13 +16,11 @@ class Trie{
     public void add(String s){//add s to the trie
         Node[] curr = head.children;
         for(int i=0; i<s.length(); ++i){
-            if(curr[s.charAt(i)-'a']==null){
+            if(curr[s.charAt(i)-'a']==null)
                 curr[s.charAt(i)-'a'] = new Node();
-            }
             curr[s.charAt(i)-'a'].pref++;
-            if(i==s.length()-1){
+            if(i==s.length()-1)
                 curr[s.charAt(i)-'a'].ends++;
-            }
             curr=curr[s.charAt(i)-'a'].children;
         }
     }
@@ -34,21 +32,18 @@ class Trie{
                 return;
             }
             curr[s.charAt(i)-'a'].pref--;
-            if(i==s.length()-1){
+            if(i==s.length()-1)
                 curr[s.charAt(i)-'a'].ends--;
-            }
             curr=curr[s.charAt(i)-'a'].children;
         }
     }
     public int haveAsPref(String s){//returns # strings with s as a prefix
         Node[] curr = head.children;
         for(int i=0; i<s.length(); ++i){
-            if(curr[s.charAt(i)-'a']==null){
+            if(curr[s.charAt(i)-'a']==null)
                 return 0;
-            }
-            if(i==s.length()-1){
+            if(i==s.length()-1)
                 return curr[s.charAt(i)-'a'].pref;
-            }
             curr=curr[s.charAt(i)-'a'].children;
         }
         return 0;
@@ -56,19 +51,16 @@ class Trie{
     public int LCP(String s, boolean isInTrie){//returns LCP of s and some string in the trie (other than s if s is in the trie)
         Node[] curr = head.children;
         for(int i=0; i<s.length(); ++i){
-            if(curr[s.charAt(i)-'a']==null || (isInTrie && curr[s.charAt(i)-'a'].pref<2)){
+            if(curr[s.charAt(i)-'a']==null || (isInTrie && curr[s.charAt(i)-'a'].pref<2))
                 return i;
-            }
             curr=curr[s.charAt(i)-'a'].children;
         }
         return s.length();
     } 
     private boolean contains(int k, int l, Node curr){
         if(curr==null) return false;
-        if(curr.pref>=k && l==1){
-            return true;
-        }
-        if(l==1) return false;
+        if(l==1)
+            return curr.pref>=k;
         boolean ans = false;
         for(int i=0; i<26; ++i){
             ans|=contains(k,l-1,curr.children[i]);
@@ -77,9 +69,8 @@ class Trie{
     }
     public boolean contains(int k, int l){//returns if there are >=k strings with LCP>=l
         boolean ans=false;
-        for(int i=0; i<26; ++i){
+        for(int i=0; i<26; ++i)
             ans|=contains(k,l,head.children[i]);
-        }
         return ans;
     }
     private int prefixes(Node node){
