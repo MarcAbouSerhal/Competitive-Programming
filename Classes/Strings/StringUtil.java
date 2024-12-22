32e80ccba43a4401c89446d9875807facbebfaea
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2024-12-22 12:56:38 +0200

    added function i'll never use to StringUtil.java

--- Classes/Strings/StringUtil.java
+++ Classes/Strings/StringUtil.java
@@ -58,4 +58,26 @@ class StringUtil{
         }
         return ap == a.length(); 
     } 
+    public static String[] letter = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    // returns all permutations in lexicographical order
+    public static ArrayList<String> permutations(int n, int[] count){
+        if(n == 1){
+            for(int i = 0; i < 26; ++i)
+                if(count[i] > 0){
+                    ArrayList<String> res = new ArrayList<>();
+                    res.add(""+(char)(i+'a'));
+                    return res;
+                }
+            return null;
+        }
+        ArrayList<String> res = new ArrayList<>();
+        for(int i = 0; i < 26; ++i)
+            if(count[i] > 0){
+                count[i]--;
+                for(String s: permutations(n-1, count))
+                    res.add(letter[i] + s);
+                count[i]++;
+            }
+        return res;
+    }
 }
