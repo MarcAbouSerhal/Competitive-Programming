963f4d9f8730fac5dc2ce888c229fec12d7bb9f2
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2024-12-23 21:27:27 +0200

    added Berlekamp-Massey Matrix.java

--- Classes/Math/Matrix.java
+++ Classes/Math/Matrix.java
@@ -19,6 +19,7 @@ class Matrix{
         // (  0   0   0   0   ...   1   0  )
         // (  0   0   0   0   ...   0   1  )
         // ( ck  ck-1 .....   ...   c2  c1 )
+        // or generally, if c is the characteristic polynomial of T
         int k = c.length;
         long[][] ans = new long[k][]; 
         ans[0] = xPowNModG(n, c, k, mod);
@@ -54,5 +55,49 @@ class Matrix{
             }
         for(int i=0; i<k; ++i) half[i] = (last[i]+mod)%mod;
         return half;
+    }
+    private final static int max(int a, int b){ return a > b ? a : b; }
+    private final static long pow(long x, long n, long m){
+        if(n == 0) return 1;
+        long res =  pow(x, n >> 1, m);
+        res = (res * res) % m;
+        if(n % 2 == 1) res = (res * x) % m;
+        return res;
+    }
+    public static long[] berlekampMassey(long[] s, long m){
+        // returns smallest possible recurrence for s
+        long[] c = new long[0], oldC = new long[0];
+        int f = -1;
+        for(int i = 0; i < s.length; ++i){
+            long delta = s[i];
+            for(int j = 1; j <= c.length; ++j)
+                delta = (delta + m - c[j - 1] * s[i - j]) % m; 
+            if(delta == 0) continue;
+            if(f == -1){
+                c = new long[i + 1];
+                f = i;
+            }
+            else{
+                long[] d = new long[oldC.length + 1];
+                d[0] = 1;
+                for(int j = 1; j <= oldC.length; ++j)
+                    d[j] = (m - oldC[j - 1]) % m;
+                long df1 = 0;
+                for(int j = 0; j < d.length; ++j)
+                    df1 = (df1 + d[j] * s[f - j]) % m;
+                long coef = (delta * pow(df1, m-2, m)) % m; 
+                for(int j = 0; j < d.length; ++j) d[j] = (d[j] * coef) % m;
+                long[] temp = c;
+                // imagine there are i - f - 1 zeros to the left of d
+                c = new long[max(c.length, d.length + i - f - 1)];
+                for(int j = 0; j < temp.length; ++j) c[j] = temp[j];
+                for(int j = 0; j < d.length; ++j) c[i - f - 1 + j] = (c[i - f - 1 + j] + d[j] + m) % m;
+                if(i - temp.length > f - oldC.length){
+                    oldC = temp;
+                    f = i;
+                }
+            }
+        }
+        return c;
     }
 }
