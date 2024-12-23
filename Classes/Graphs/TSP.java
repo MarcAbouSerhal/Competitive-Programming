aad95d24ec2cb2df07a1fc8147a2d505ec75c1d2
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2024-12-23 02:03:14 +0200

    optimizations to TSP.java

--- Classes/Graphs/TSP.java
+++ Classes/Graphs/TSP.java
@@ -10,9 +10,9 @@ class TSP{
     // assumes start is 0
     public final static long tsp(long[][] d){
         int n = d.length;
-        long[][] g = new long[(1 << (n - 1)) - 1][n - 1];
+        long[][] g = new long[1 << (n - 1)][n - 1];
         for(int k = 0; k < n - 1; ++k)
-            g[(1 << k) - 1][k] = d[0][k + 1];
+            g[1 << k][k] = d[0][k + 1];
         int[][] masks = new int[(1 << (n - 1))][2];
         for(int i = 0; i < masks.length; ++i){
             masks[i][0] = i;
@@ -24,15 +24,19 @@ class TSP{
             if((mask & (mask - 1)) == 0) continue;
             for(int k = 0; k < n - 1; ++k)
                 if((mask & (1 << k)) != 0){
-                    g[mask - 1][k] = inf;
-                    for(int m = 0; m < n - 1; ++m)
-                        if(k != m && (mask & (1 << m))!= 0)
-                            g[mask - 1][k] = min(g[mask - 1][k], g[(mask ^ (1 << k)) - 1][m] + d[m + 1][k + 1]);
+                    g[mask][k] = inf;
+                    int m;
+                    for(m = 0; m < k; ++m)
+                        if((mask & (1 << m))!= 0)
+                            g[mask][k] = min(g[mask][k], g[mask ^ (1 << k)][m] + d[m + 1][k + 1]);
+                    for(m = k + 1; m < n - 1; ++m)
+                        if((mask & (1 << m))!= 0)
+                            g[mask][k] = min(g[mask][k], g[mask ^ (1 << k)][m] + d[m + 1][k + 1]);
                 } 
         }
         long opt = inf;
         for(int i = 0; i < n - 1; ++i)
-            opt = min(opt, g[(1 << (n - 1)) - 2][i] + d[i + 1][0]);
+            opt = min(opt, g[(1 << (n - 1)) - 1][i] + d[i + 1][0]);
         return opt;
     }
 }
