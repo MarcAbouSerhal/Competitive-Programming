1371d74612a0ad6d44a7ecc9581dd9edc9a40631
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2024-12-29 00:57:45 +0200

    finished WaveletTree v1.0, more optimizations + fixed getLeq and getEq with optimization

--- Classes/Range Queries/WaveletTree.java
+++ Classes/Range Queries/WaveletTree.java
@@ -1,127 +1,117 @@
-// NOTE: EQ NOT TESTED
 class WaveletTree{
-    private static class List{ // reduce overhead of ArrayList<Integer>
+    private static final class List{ // reduce overhead of ArrayList<Integer>
         int n = 0;
-        int[] get;
+        final int[] get;
         public List(int n){ get = new int[n]; }
-        public void add(int x){ get[n++] = x; }
-        public void clear() { n = 0; }
+        public final void add(int x){ get[n++] = x; }
     }
-    long[] a;
-    private class Node{
+    private final long[] a;
+    private final class Node{
         long low, high;
         Node left, right;
-        int[] b;
+        final int[] b;
         public Node(List indices, long lo, long hi){
             low = lo;
             high = hi;
             int n = indices.n;
             b = new int[n];
-            if(n == 0 || low == high) return;
-
+            if(n == 0) return;
+            if(low == high){
+                for(int i = 0; i < n; ++i) b[i] = i + 1;
+                return;
+            }
+            long min = high, max = low;
+            for(int i: indices.get){
+                min = min < a[i] ? min : a[i];
+                max = max > a[i] ? max : a[i];
+            }
             long mid = (low + high) / 2;
-            List leftIndices = new List(n), rightIndices = new List(n);
+            // compress travels by making sure this node has 2 children or no children
+            while(low != high && (max <= mid || mid < min)){
+                if(mid < min) low = mid + 1;
+                else high = mid;
+                mid = (low + high) / 2;
+            }
+            indices.n = 0; // use indices as leftIndices for better memory usage
+            List rightIndices = new List(n);
             
             int i0 = indices.get[0];
-            if(a[i0] <= mid){
+            if(a[i0] > mid) rightIndices.add(i0);
+            else{
                 b[0] = 1;
-                leftIndices.add(i0);
+                indices.add(i0);
             }
-            else rightIndices.add(i0);
             for(int i = 1; i < n; ++i){
                 int j = indices.get[i];
                 b[i] = b[i - 1];
-                if(a[j] <= mid){
-                    leftIndices.add(j);
+                if(a[j] > mid) rightIndices.add(j);
+                else{
+                    indices.add(j);
                     ++b[i];
                 }
-                else rightIndices.add(j);
             }
-            // compressing travels
-            // while this has 1 child, make it that child
-            while((b[n - 1] == 0 || b[n - 1] == n) && low != high){
-                if(b[n - 1] == 0) low = mid + 1;
-                else high = mid;
-                mid = (low + high) / 2;
-                leftIndices.clear();
-                rightIndices.clear();
-                
-                i0 = indices.get[0];
-                if(a[i0] <= mid){
-                    b[0] = 1;
-                    leftIndices.add(i0);
-                }
-                else{
-                    b[0] = 0; // old b[0] value might be 1
-                    rightIndices.add(i0);
-                }
-                for(int i = 1; i < n; ++i){
-                    int j = indices.get[i];
-                    b[i] = b[i - 1];
-                    if(a[j] <= mid){
-                        leftIndices.add(j);
-                        ++b[i];
-                    }
-                    else rightIndices.add(j);
-                }
-            }
-            left = new Node(leftIndices, low, mid);
+            left = new Node(indices, low, mid);
             right = new Node(rightIndices, mid + 1, high);
         }
     }
-    Node root;
-    public WaveletTree(long[] a, int min, int max){
+    private final Node root;
+    public WaveletTree(long[] a, long min, long max){
         this.a = a;
         int n = a.length;
         List indices = new List(n);
         for(int i = 0; i < n; ++i) indices.add(i);
         root = new Node(indices, min, max);
     }
-    private int getLeq(int r, long x){
-        if(r == -1) return 0;
+    public final int getLeq(int l, int r, long x){
         Node node = root;
         int count = 0;
-        while(node != null && r != -1){
+        while(node != null && l <= r){
             long m = (node.low + node.high) / 2;
             if(m == x)
-                return count + node.b[r];
+                return count + (l == 0 ? node.b[r] : node.b[r] - node.b[l - 1]);
             else if(m < x){
-                count += node.b[r];
+                if(l == 0){
+                    count += node.b[r];
+                    l = l - node.b[l] + node.b[0];
+                }
+                else{
+                    count += node.b[r] - node.b[l - 1];
+                    l = l - node.b[l] + node.b[l] - node.b[l - 1];
+                }
                 r = r - node.b[r];
                 node = node.right;
             }
             else{
                 r = node.b[r] - 1;
+                l = node.b[l] - (l == 0 ? node.b[0] : node.b[l] - node.b[l - 1]);
                 node = node.left;
             }
         }
         return count;
     }
-    public int getLeq(int l, int r, long x){
-        return getLeq(r, x) - getLeq(l - 1, x);
-    }
-    private int getEq(int r, long x){
-        if(r == -1) return 0;
+    public final int getEq(int l, int r, long x){
         Node node = root;
-        while(node != null && r != -1){
+        while(node != null && l <= r){
             long m = (node.low + node.high) / 2;
-            if(m == x)
-                return node.b[r];
+            if(m == x && node.low == m)
+                return l == 0 ? node.b[r] : node.b[r] - node.b[l - 1];
             else if(m < x){
+                if(l == 0)
+                    l = l - node.b[l] + node.b[0];
+                else
+                    l = l - node.b[l] + node.b[l] - node.b[l - 1];
                 r = r - node.b[r];
                 node = node.right;
             }
             else{
                 r = node.b[r] - 1;
+                l = node.b[l] - (l == 0 ? node.b[0] : node.b[l] - node.b[l - 1]);
                 node = node.left;
             }
         }
         return 0;
     }
-    public int getEq(int l, int r, long x){
-        return getEq(r, x) - getEq(l - 1, x);
-    }
-    public long kthInRange(int l, int r, int k){
+    public final long kthInRange(int l, int r, int k){
         Node node = root;
         while(true){
             if(node.low == node.high) return node.high;
