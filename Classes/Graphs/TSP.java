0886f81fbcde1932f7b041ce1f1038047e9a8989
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2024-12-31 18:37:37 +0200

    big optimization to TSP (no more going through masks in order of activated bits as its useless)

--- Classes/Graphs/TSP.java
+++ Classes/Graphs/TSP.java
@@ -1,26 +1,13 @@
 class TSP{
-    private final static long inf = Long.MAX_VALUE;
-    private final static long min(long x, long y){ return x < y ? x : y; }
-    private final static int bitCount(int x){
-        int ans = 0;
-        for(int i = 0; i < 21; ++i)
-            if((x & (1 << i)) != 0) ++ans;
-        return ans;
-    }
+    private final static int inf = Integer.MAX_VALUE;
+    private final static int min(int x, int y){ return x < y ? x : y; }
     // assumes start is 0
-    public final static long tsp(long[][] d){
+    public static final int tsp(int[][] d){
         int n = d.length;
-        long[][] g = new long[1 << (n - 1)][n - 1];
+        int[][] g = new int[1 << (n - 1)][n - 1];
         for(int k = 0; k < n - 1; ++k)
             g[1 << k][k] = d[0][k + 1];
-        int[][] masks = new int[(1 << (n - 1))][2];
-        for(int i = 0; i < masks.length; ++i){
-            masks[i][0] = i;
-            masks[i][1] = bitCount(i);
-        }
-        Arrays.sort(masks, (x,y) -> x[1] - y[1]);
-        for(int[] maskp: masks){
-            int mask = maskp[0];
+        for(int mask = 3; mask < 1 << (n - 1); ++mask){
             if((mask & (mask - 1)) == 0) continue;
             for(int k = 0; k < n - 1; ++k)
                 if((mask & (1 << k)) != 0){
@@ -34,7 +21,7 @@ class TSP{
                             g[mask][k] = min(g[mask][k], g[mask ^ (1 << k)][m] + d[m + 1][k + 1]);
                 } 
         }
-        long opt = inf;
+        int opt = inf;
         for(int i = 0; i < n - 1; ++i)
             opt = min(opt, g[(1 << (n - 1)) - 1][i] + d[i + 1][0]);
         return opt;
