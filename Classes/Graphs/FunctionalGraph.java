e0b14da41abed4e6d4eb3e07d315a8c91135aa76
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-01-12 10:33:08 +0200

    update to FunctionalGraph

--- Classes/Graphs/FunctionalGraph.java
+++ Classes/Graphs/FunctionalGraph.java
@@ -6,20 +6,25 @@ class FunctionalGraph{
     // returns vertices of cycle with that id
     final int[] tree_id;
     // -1  : part of a cycle
-    // >=0 : id of the tree the vertex belongs to
+    // >=0 : id of the tree the vertex belongs to 
     final int[] cycle_index;
-    // given a vertex u, returns its index in the cycle it belongs to (cycle[id[u]].index(u))
+    // given a vertex u that belongs to a cycle, returns its index in the cycle it belongs to (cycle[id[u]].index(u))
     final int[] leading_to;
-    // given a vertex u, return the first vertex of the cycle it leads to (if it belongs to a tree, what vertex of a cycle that tree points to)
+    // given a vertex u that belongs to a tree, return  what vertex of a cycle that tree points to
     final int[] cycle_dist;
     // given a vertex u, returns distance from u to the cycle it leads to
     // cycle_dist[u] = 0 iff u is part of a cycle
+    final int log;
+    final int[][] nxt;
     public FunctionalGraph(int[] next){
         final int n = next.length;
         final ArrayList<Integer>[] before = new ArrayList[n];
         cycle_id = new int[n]; for(int u = 0; u < n; ++u){ cycle_id[u] = -2; before[u] = new ArrayList<>(); }
         cycle_index = new int[n];
         cycle = new ArrayList[n];
+        cycle_dist = new int[n];
+        tree_id = new int[n];
+        leading_to = new int[n]; 
         int cycles = 0;
         for(int u = 0; u < n; ++u){
             before[next[u]].add(u);
@@ -28,7 +33,7 @@ class FunctionalGraph{
             if(next[u] == u){
                 ArrayList<Integer> vertices = new ArrayList<>();
                 vertices.add(u);
-                cycle_index[u] = 0;
+                tree_id[u] = -1;
                 cycle_id[u] = cycles;
                 cycle[cycles++] = vertices;
                 continue;
@@ -42,10 +47,12 @@ class FunctionalGraph{
                 path.add(at);
             }
             ArrayList<Integer> vertices = new ArrayList<>();
+            int firstOfCycle = next[at];
             boolean in_cycle = false;
             for (int i : path) { 
-                in_cycle = in_cycle | i == next[at];
+                in_cycle = in_cycle || i == firstOfCycle;
                 if (in_cycle){
+                    tree_id[i] = -1;
                     cycle_index[i] = vertices.size();
                     vertices.add(i);
                 }
@@ -53,14 +60,11 @@ class FunctionalGraph{
             }
             cycle[cycles++] = vertices;
         }
-        cycle_dist = new int[n];
-        tree_id = new int[n]; for(int u = 0; u < n; ++u) tree_id[u] = -1;
-        leading_to = new int[n]; for(int u = 0; u < n; ++u) leading_to[u] = next[u];
         cycles = 0; // here cycles counts number of trees, just trying to use less memory
         int curr, destination;
         for(int u = 0; u < n; ++u){
             if(cycle_id[next[u]] == -1 || cycle_id[u] != -1) continue;
-            destination = next[u];
+            leading_to[u] = destination = next[u];
             cycle_dist[u] = 1;
             tree_id[u] = cycles;
             Stack s = new Stack(n);
@@ -73,6 +77,28 @@ class FunctionalGraph{
             }
             ++cycles;
         }
+        // this last part is for kth next (comment it out if not used)
+        log = 32 - Integer.numberOfLeadingZeros(n);
+        nxt = new int[n][log];
+        for(int i = 0; i < n; ++i) nxt[i][0] = next[i];
+        for(int j = 1; j < log; ++j)
+            for(int i = 0; i < n; ++i)
+                nxt[i][j] = nxt[nxt[i][j - 1]][j - 1];
+    }
+    // (O(log(n)) if u was and stays in a tree, otherwise O(1))
+    public final int kthNext(int u, int k){
+        if(cycle_dist[u] > k){
+            for(int i = 0; i < log; ++i)
+                if((k & (1 << i)) != 0)
+                    u = nxt[u][i];
+            return u;
+        }
+        if(cycle_dist[u] > 0){
+            k -= cycle_dist[u];
+            u = leading_to[u];
+        }
+        final ArrayList<Integer> cyc = cycle[cycle_id[u]];
+        return cyc.get((cycle_index[u] + k) % cyc.size());
     }
     private static final class Stack{
         private final int[] s;
