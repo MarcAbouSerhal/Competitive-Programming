96dcbe46be81fdf01a35dfcc3e6a232903cf0b5d
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-01-13 21:16:33 +0200

    almost finalized EulerDirected (simplified checking into helper function, added path from s to e (with s==e edgecase), and path starting with s)

--- Classes/Graphs/EulerDirected.java
+++ Classes/Graphs/EulerDirected.java
@@ -1,23 +1,38 @@
 class EulerDirected{
     // gets euler cycle if it exists (indegree[u] = outdegree[u] for every u), and null if it doesn't exist  (O(n + m))
-    public final static int[] getCycle(ArrayList<Integer>[] adj_, int start){
-        final int n = adj_.length;
+    public final static int[] getCycle(ArrayList<Integer>[] adj, int start){
+        final int n = adj.length;
         final int[] indegree = new int[n];
         int m = 0;
         for(int u = 0; u < n; ++u){
-            m += adj_[u].size();
-            for(int v: adj_[u])
+            m += adj[u].size();
+            for(int v: adj[u])
                 ++indegree[v];
         }
         for(int u = 0; u < n; ++u)
-            if(indegree[u] != adj_[u].size()) 
+            if(indegree[u] != adj[u].size()) 
                 return null;
-        return hierholzer(adj_, m, start);
+        return hierholzer(adj, m, start);
     }
-    // gets euler path if it exists ( atmost one u s.t. outdegree[u] - indegree[u] = 1, and 
+    // gets euler path if it exists (atmost one u s.t. outdegree[u] - indegree[u] = 1, and 
     // atmost v s.t. indegree[v] - outdegree[v] = 1, with rest having indegree[w] = outdegree[w] for every other vertex)
     // and null if it doesn't exist (O(n + m))
-    public final static int[] getPath(ArrayList<Integer>[] adj_){
+    public final static int[] getPath(ArrayList<Integer>[] adj){
+        final int[] info = helper(adj);
+        return info == null ? null : hierholzer(adj, info[2], info[0]);
+    }
+    // gets euler path from s to e if it exists and null if it doesn't exist (O(n + m))
+    public final static int[] getPath(ArrayList<Integer>[] adj, int s, int e){
+        final int[] info = helper(adj);
+        return info == null ? null : (info[0] == s && info[1] == e) || (s == e && info[0] == info[1]) ? hierholzer(adj, info[2], s) : null;
+    }
+    // gets euler path starting from s if it exists and null if it doesn't exist (O(n + m))
+    public final static int[] getPath(ArrayList<Integer>[] adj, int s){
+        final int[] info = helper(adj);
+        return info == null ? null : info[0] == s || info[0] == info[1] ? hierholzer(adj, info[2], s) : null;
+    }
+    // returns {potential start, potential end, number of edges}
+    private final static int[] helper(ArrayList<Integer>[] adj_){
         final int n = adj_.length;
         final int[] indegree = new int[n];
         int m = 0;
@@ -39,7 +54,7 @@ class EulerDirected{
             }
             else return null;
         }
-        return hierholzer(adj_, m, start == -1 ? 0 : start);
+        return start == -1 ? new int[] {0, 0, m} : new int[] {start, end, m};
     }
     private final static int[] hierholzer(ArrayList<Integer>[] adj_, int m, int start){
         final Stack[] adj = copy(adj_);
