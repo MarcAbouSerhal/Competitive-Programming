42173637047664af211cc71aaf0555f9b1b0ab81
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-01-17 10:19:59 +0200

    small change to MaxSubarraySegmentTree

--- Classes/Range Queries/MaxSubarraySegmentTree.java
+++ Classes/Range Queries/MaxSubarraySegmentTree.java
@@ -1,10 +1,9 @@
-// find max sum (non-empty) subarray
-// remove comments if you care about bound of max sum subarray
+// finds max sum (non-empty) subarray
 class MaxSubarraySegmentTree{
     private final Node[] tree;
     private int leaves;
     // (O(n))
-    public MaxSubarraySegmentTree(int[] a){
+    public MaxSubarraySegmentTree(long[] a){
         leaves = a.length;
         if((leaves & (leaves - 1)) != 0)
             leaves = Integer.highestOneBit(leaves) << 1;
@@ -17,7 +16,7 @@ class MaxSubarraySegmentTree{
         return get(l, r, 0, 0, leaves - 1);
     }
     // sets a[x] to v (O(log(n)))
-    public final void set(int x, int v){
+    public final void set(int x, long v){
         x += leaves - 1;
         tree[x]= init(v , x - leaves + 1);
         while(x != 0){
@@ -25,12 +24,13 @@ class MaxSubarraySegmentTree{
             tree[x] = op(tree[(x << 1) + 1],tree[(x + 1) << 1]);
         }
     }
-    private static final Node init(int x, int i){
+    private static final Node init(long x, int i){
         Node result = new Node();
         // result.l = result.r = result.prefIndex = result.sufIndex = i;
         result.sum = result.bestSum = result.pref = result.suf = x;
         return result;
     }
+    private static final long max(long x, long y){ return x > y ? x : y; }
     private static final Node op(Node a, Node b){
         if(a == null) return b;
         if(b == null) return a;
@@ -38,40 +38,46 @@ class MaxSubarraySegmentTree{
  
         result.sum = a.sum + b.sum;
  
-        if(a.sum + b.pref > a.pref){
-            result.pref = a.sum + b.pref;
-            // result.prefIndex = b.prefIndex;
-        }
-        else{
-            result.pref = a.pref;
-            // result.prefIndex = a.prefIndex;
-        }
+        // use this if you want bounds [l...r] of the max subarray
+        // if(a.sum + b.pref > a.pref){
+        //     result.pref = a.sum + b.pref;
+        //     result.prefIndex = b.prefIndex;
+        // }
+        // else{
+        //     result.pref = a.pref;
+        //     result.prefIndex = a.prefIndex;
+        // }
  
-        if(a.suf + b.sum > b.suf){
-            result.suf = a.suf + b.sum;
-            // result.sufIndex = a.sufIndex;
-        }
-        else{
-            result.suf = b.suf;
-            // result.sufIndex = b.sufIndex;
-        }
+        // if(a.suf + b.sum > b.suf){
+        //     result.suf = a.suf + b.sum;
+        //     result.sufIndex = a.sufIndex;
+        // }
+        // else{
+        //     result.suf = b.suf;
+        //     result.sufIndex = b.sufIndex;
+        // }
  
-        if(a.suf + b.pref > (a.bestSum > b.bestSum ? a.bestSum : b.bestSum)){
-            result.bestSum = a.suf + b.pref;
-            // result.l = a.sufIndex;
-            // result.r = b.prefIndex;
-        }
-        else if(a.bestSum > b.bestSum){
-            result.bestSum = a.bestSum;
-            // result.l = a.l;
-            // result.r = a.r;
-        }
-        else{
-            result.bestSum = b.bestSum;
-            // result.l = b.l;
-            // result.r = b.r;
-        }
+        // if(a.suf + b.pref > (a.bestSum > b.bestSum ? a.bestSum : b.bestSum)){
+        //     result.bestSum = a.suf + b.pref;
+        //     result.l = a.sufIndex;
+        //     result.r = b.prefIndex;
+        // }
+        // else if(a.bestSum > b.bestSum){
+        //     result.bestSum = a.bestSum;
+        //     result.l = a.l;
+        //     result.r = a.r;
+        // }
+        // else{
+        //     result.bestSum = b.bestSum;
+        //     result.l = b.l;
+        //     result.r = b.r;
+        // }
  
+        // use this if you only want the sum of the max subarray
+        result.pref = max(a.pref, a.sum + b.pref);
+        result.suf = max(b.suf, a.suf + b.sum);
+        result.bestSum = max(max(a.bestSum, b.bestSum), a.suf + b.pref);
+
         return result;
     }
     private final Node get(int l, int r, int x,int lx, int rx){
@@ -82,6 +88,7 @@ class MaxSubarraySegmentTree{
     }
 }
 class Node{
+    // comment this out if you want bounds [l...r] of the max subarray
     // int l, r, prefIndex, sufIndex;
-    int sum, bestSum, pref, suf;
+    long sum, bestSum, pref, suf;
 }
