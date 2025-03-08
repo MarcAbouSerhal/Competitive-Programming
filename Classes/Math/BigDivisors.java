dc3ac25b7ff60e752e5a3b02ec9d9bfbdb2bf803
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-03-08 21:00:18 +0200

    BigDivisors uses int instead of long + Pair is now a public inner class

--- Classes/Math/BigDivisors.java
+++ Classes/Math/BigDivisors.java
@@ -1,28 +1,27 @@
 // use this for x >= 1e7 
 class BigDivisors{
-    private final static class Pair{
-        final long x;
-        final int y;
-        public Pair(long x, int y){ this.x = x; this.y = y; }
+    final static class Pair{
+        final int x, y;
+        public Pair(int x, int y){ this.x = x; this.y = y; }
     }
-    private final ArrayList<Long> primes = new ArrayList<>();
+    private final ArrayList<Integer> primes = new ArrayList<>();
     // here n is the square root of the max value of x
     public BigDivisors(int n){ 
         BitSet isPrime = new BitSet(n + 1);
         isPrime.set(0, n);
-        primes.add(2l);
+        primes.add(2);
         for(int i = 4; i <= n; i += 2) isPrime.clear(i);
         for(int i = 3; i * i <= n; i += 2)
             if(isPrime.get(i))
                 for(int j = i * i; j <= n; j += i << 1)
                     isPrime.clear(j);
         for(int i = 3; i <= n; i += 2)
-            if(isPrime.get(i)) primes.add((long)i);
+            if(isPrime.get(i)) primes.add(i);
     }
     // returns list of (p,e) where x = sum(p^e) (O(sqrt(x) / log(x)))
-    public final ArrayList<Pair> primeFactors(long x){
+    public final ArrayList<Pair> primeFactors(int x){
         ArrayList<Pair> res = new ArrayList<>();
-        for(long y: primes){
+        for(int y: primes){
             if(y * y > x) break;
             if(x % y != 0) continue;
             int cnt = 0;
@@ -36,14 +35,14 @@ class BigDivisors{
         return res;
     }
     // returns unsorted list of all divisors of x (O(sqrt(x) / log(x) + d(x)))
-    public final ArrayList<Long> divisors(long x){
+    public final ArrayList<Integer> divisors(int x){
         ArrayList<Pair> pfs = primeFactors(x);
-        ArrayList<Long> ans = new ArrayList<>();
-        f(1l, 0, pfs, ans);
-        ans.add(1l);
+        ArrayList<Integer> ans = new ArrayList<>();
+        f(1, 0, pfs, ans);
+        ans.add(1);
         return ans;
     }
-    private static final void f(long x, int i, ArrayList<Pair> pfs, ArrayList<Long> ans){
+    private static final void f(int x, int i, ArrayList<Pair> pfs, ArrayList<Integer> ans){
         if(i == pfs.size()) return;
         f(x, i + 1, pfs, ans);
         for(int cnt = 1; cnt <= pfs.get(i).y; ++cnt){
