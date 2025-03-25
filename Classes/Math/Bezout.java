2133ec9a89b80143bf5f83eb2d43fab5ed46e08c
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-03-25 14:42:22 +0200

    added Bezout

---
+++ Classes/Math/Bezout.java
@@ -0,0 +1,44 @@
+class Bezout{
+    public static long gcd;
+    // returns {x, y} such that ax + by = gcd(a, b) (O(log(min(a,b))))
+    public static final long[] extendedEuclidean(long a, long b){
+        long x = 1, y = 0, x1 = 0, y1 = 0, temp;
+        while(b != 0){
+            long q = a / b;
+            temp = x;
+            x = x1;
+            x1 = temp - q * x1;
+            temp = y;
+            y = y1;
+            y1 = temp - q * y1;
+            temp = b;
+            a = b;
+            b = temp - q * b;
+        }
+        gcd = a;
+        return new long[] {x, y};
+    }
+    // return x such that a[0]x[0] + a[1]x[1] + ... + a[n - 1]x[n - 1] = gcd(a)
+    public static final long[] linearEquation(long[] a){
+        int n = a.length;
+        if(n == 1) return new long[] {a[0] > 0 ? 1 : -1};
+        else if(n == 2) return extendedEuclidean(a[0], a[1]);
+        else{
+            long[] x = new long[n];
+            long[] xy = extendedEuclidean(a[0], a[1]);
+            x[0] = xy[0];
+            x[1] = xy[1];
+            long[] leftProduct = new long[n - 2];
+            for(int i = 2; i < n; ++i){
+                xy = extendedEuclidean(gcd, a[i]);
+                leftProduct[i - 2] = xy[0];
+                x[i] = xy[1];
+            }
+            for(int i = n - 4; i >= 0; --i) 
+                leftProduct[i] *= leftProduct[i + 1];
+            x[0] *= leftProduct[0];
+            for(int i = 1; i < n - 1; ++i) x[i] *= leftProduct[i - 1];
+            return x;
+        }
+    }
+}
