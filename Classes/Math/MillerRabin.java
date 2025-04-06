c00ae05ed9f7c798855cf663ec9570a293b11687
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-04-06 16:02:25 +0300

    fixed overflow in MillerRabin

--- Classes/Math/MillerRabin.java
+++ Classes/Math/MillerRabin.java
@@ -3,22 +3,17 @@ class MillerRabin{
     private static final int[] bases = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
     // these bases work for n <= 1e18, if n is bigger, might have to add more
     // Note: only necessary to test all bases a <= 2ln^2(n)
-    private static final long pow(long x, long y, long m) {
-        long result = 1;
-        x %= m;
-        while(y != 0) {
-            if((y & 1) != 0) result = (result * x) % m;
-            x = (x * x) % m;
-            y >>= 1;
-        }
-        return result;
-    }
     private static final boolean checkComposite(long n, long a, long d, int s) {
-        long x = pow(a, d, n);
-        if(x == 1 || x == n - 1) return false;
-        for(int r = 1; r < s; ++r) {
-            x = (x * x) % n;
-            if(x == n - 1) return false;
+        BigInteger bigN = BigInteger.valueOf(n);
+        BigInteger bigA = BigInteger.valueOf(a).mod(bigN);
+        BigInteger bigD = BigInteger.valueOf(d);
+        BigInteger x = bigA.modPow(bigD, bigN);
+
+        if (x.equals(BigInteger.ONE) || x.equals(bigN.subtract(BigInteger.ONE))) return false;
+
+        for (int r = 1; r < s; ++r) {
+            x = x.multiply(x).mod(bigN);
+            if (x.equals(bigN.subtract(BigInteger.ONE))) return false;
         }
         return true;
     }
