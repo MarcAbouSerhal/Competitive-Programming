f254c4f40331d18056cbdd142f0322e52ca894c4
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-05-06 22:07:45 +0300

    FastIO now uses buffer with char[] , use nextString() for String return type

--- Classes/FastIO.java
+++ Classes/FastIO.java
@@ -1,40 +1,91 @@
 class FastIO{ 
     private final BufferedOutputStream bos;
-    private final BufferedReader br; 
-    private StringTokenizer st; 
+    private final InputStream in = System.in;
+    private final byte[] buffer = new byte[1 << 16];
+    private int pointer = 0, bytesRead = 0;
     public FastIO() { 
         bos = new BufferedOutputStream(System.out);
-        br = new BufferedReader(new InputStreamReader(System.in)); 
     } 
-    public final String next() throws Exception { 
-        while (st == null || !st.hasMoreElements())
-            st = new StringTokenizer(br.readLine());
-        return st.nextToken(); 
-    } 
-    public final int nextInt() throws Exception { 
-        final String s = next();
-        final int off = s.charAt(0) == '-' ? 0 : -1;
-        int answer = 0, pow = - (off << 1) - 1;
-        for(int i = s.length() - 1; i > off; --i){
-            answer += (s.charAt(i) - '0') * pow;
-            pow *= 10;
+    private byte read() throws IOException {
+        if(pointer >+ bytesRead) {
+            pointer = 0;
+            if((bytesRead = in.read(buffer)) == -1) return -1;
         }
-        return answer;
-    } 
-    public final long nextLong() throws Exception { 
-        final String s = next();
-        final int off = s.charAt(0) == '-' ? 0 : -1;
-        long answer = 0, pow = - (off << 1) - 1;
-        for(int i = s.length() - 1; i > off; --i){
-            answer += (s.charAt(i) - '0') * pow;
-            pow *= 10;
+        return buffer[pointer++];
+    }
+    public final char[] next() throws IOException { 
+        byte b;
+        while((b = read()) <= ' ' && b != -1);
+        char[] token = new char[10];
+        int len = 0;
+        do {
+            if(len == token.length) {
+                char[] newToken = new char[len << 1];
+                for(int i = 0; i < len; ++i) newToken[i] = token[i];
+                token = newToken;
+            }
+            token[len++] = (char)b;
+        } while((b = read()) > ' ');
+        if(len == token.length) return token;
+        else {
+            char[] resizedToken = new char[len];
+            for(int i = 0; i < len; ++i) resizedToken[i] = token[i];
+            return resizedToken;
         }
-        return answer;
     } 
-    public final double nextDouble() throws Exception { return Double.parseDouble(next()); } 
-    public final String nextLine() throws Exception { 
-        if(st.hasMoreTokens()) return st.nextToken("\n"); 
-        else return br.readLine(); 
+    public final String nextString() throws IOException { return new String(next()); }
+    public final int nextInt() throws IOException { 
+        int num = 0;
+        byte b;
+        boolean neg = false;
+        while ((b = read()) <= ' ') {
+            if (b == -1) return -1;
+        }
+        if (b == '-') {
+            neg = true;
+            b = read();
+        }
+        do {
+            num = num * 10 + (b - '0');
+        } while ((b = read()) >= '0' && b <= '9');
+        return neg ? -num : num;
+    } 
+    public final long nextLong() throws IOException { 
+        long num = 0;
+        byte b;
+        boolean neg = false;
+        while ((b = read()) <= ' ') {
+            if (b == -1) return -1;
+        }
+        if (b == '-') {
+            neg = true;
+            b = read();
+        }
+        do {
+            num = num * 10 + (b - '0');
+        } while ((b = read()) >= '0' && b <= '9');
+        return neg ? -num : num;
+    } 
+    public final double nextDouble() throws Exception { return Double.parseDouble(new String(next())); } 
+    public final char[] nextLine() throws IOException { 
+        byte b = read();
+        if(b == -1) return new char[0];
+        char[] token = new char[128];
+        int len = 0;
+        do {
+            if(len == token.length) {
+                char[] newToken = new char[len << 1];
+                for(int i = 0; i < len; ++i) newToken[i] = token[i];
+                token = newToken;
+            }
+            token[len++] = (char)b;
+        } while((b = read()) != '\n' && b != -1);
+        if(len == token.length) return token;
+        else {
+            char[] resizedToken = new char[len];
+            for(int i = 0; i < len; ++i) resizedToken[i] = token[i];
+            return resizedToken;
+        }
     } 
     private static final byte[] bytes(char[] s){
         final int n = s.length;
@@ -100,27 +151,27 @@ class FastIO{
     }
     private static final byte[] TRUE = {'t', 'r', 'u', 'e'}, FALSE = {'f', 'a', 'l', 's', 'e'}, NULL = {'n', 'u', 'l', 'l'}, ENDL = "\n".getBytes(), SPACE = {' '}, ZERO = {'0'};
     private static final byte MINUS = '-';
-    public final void print(char c) throws Exception { bos.write(c); }
-    public final void println(char c) throws Exception { bos.write(c); bos.write(ENDL); }
-    public final void print(String s) throws Exception { bos.write(bytes(s)); }
-    public final void println(String s) throws Exception { bos.write(bytes(s)); bos.write(ENDL); }
-    public final void print(int x) throws Exception { bos.write(bytes(x)); }
-    public final void println(int x) throws Exception { bos.write(bytes(x)); bos.write(ENDL); }
-    public final void print(long x) throws Exception { bos.write(bytes(x)); }
-    public final void println(long x) throws Exception { bos.write(bytes(x)); bos.write(ENDL); }
-    public final void print(double x) throws Exception { bos.write(bytes(Double.toString(x))); }
-    public final void println(double x) throws Exception { bos.write(bytes(Double.toString(x))); bos.write(ENDL); }
-    public final void print(boolean b) throws Exception { bos.write(b ? TRUE : FALSE); }
-    public final void println(boolean b) throws Exception { bos.write(b ? TRUE : FALSE); bos.write(ENDL); }
-    public final void print(Object obj) throws Exception { if(obj == null) print(NULL); else print(obj.toString()); }
-    public final void println(Object obj) throws Exception { if(obj == null) println(NULL); else println(obj.toString()); }
-    public final void print(int[] array) throws Exception { for(int i: array){ bos.write(bytes(i)); bos.write(SPACE); } }
-    public final void println(int[] array) throws Exception { for(int i: array){ bos.write(bytes(i)); bos.write(SPACE); } bos.write(ENDL); }
-    public final void print(long[] array) throws Exception { for(long i: array){ bos.write(bytes(i)); bos.write(SPACE); } }
-    public final void println(long[] array) throws Exception { for(long i: array){ bos.write(bytes(i)); bos.write(SPACE); } bos.write(ENDL); }
-    public final void print(char[] s) throws Exception { bos.write(bytes(s)); }
-    public final void println(char[] s) throws Exception { bos.write(bytes(s)); bos.write(ENDL); }
-    public final void space() throws Exception { bos.write(SPACE); }
-    public final void endl() throws Exception { bos.write(ENDL); }
-    public final void flush() throws Exception { bos.flush(); }
+    public final void print(char c) throws IOException { bos.write(c); }
+    public final void println(char c) throws IOException { bos.write(c); bos.write(ENDL); }
+    public final void print(String s) throws IOException { bos.write(bytes(s)); }
+    public final void println(String s) throws IOException { bos.write(bytes(s)); bos.write(ENDL); }
+    public final void print(int x) throws IOException { bos.write(bytes(x)); }
+    public final void println(int x) throws IOException { bos.write(bytes(x)); bos.write(ENDL); }
+    public final void print(long x) throws IOException { bos.write(bytes(x)); }
+    public final void println(long x) throws IOException { bos.write(bytes(x)); bos.write(ENDL); }
+    public final void print(double x) throws IOException { bos.write(bytes(Double.toString(x))); }
+    public final void println(double x) throws IOException { bos.write(bytes(Double.toString(x))); bos.write(ENDL); }
+    public final void print(boolean b) throws IOException { bos.write(b ? TRUE : FALSE); }
+    public final void println(boolean b) throws IOException { bos.write(b ? TRUE : FALSE); bos.write(ENDL); }
+    public final void print(Object obj) throws IOException { if(obj == null) print(NULL); else print(obj.toString()); }
+    public final void println(Object obj) throws IOException { if(obj == null) println(NULL); else println(obj.toString()); }
+    public final void print(int[] array) throws IOException { for(int i: array){ bos.write(bytes(i)); bos.write(SPACE); } }
+    public final void println(int[] array) throws IOException { for(int i: array){ bos.write(bytes(i)); bos.write(SPACE); } bos.write(ENDL); }
+    public final void print(long[] array) throws IOException { for(long i: array){ bos.write(bytes(i)); bos.write(SPACE); } }
+    public final void println(long[] array) throws IOException { for(long i: array){ bos.write(bytes(i)); bos.write(SPACE); } bos.write(ENDL); }
+    public final void print(char[] s) throws IOException { bos.write(bytes(s)); }
+    public final void println(char[] s) throws IOException { bos.write(bytes(s)); bos.write(ENDL); }
+    public final void space() throws IOException { bos.write(SPACE); }
+    public final void endl() throws IOException { bos.write(ENDL); }
+    public final void flush() throws IOException { bos.flush(); }
 }
