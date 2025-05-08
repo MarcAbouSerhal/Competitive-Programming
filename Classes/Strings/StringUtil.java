ee754f1ced236fd51f19b21280b69e6ae19e6846
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-05-08 19:39:26 +0300

    reduced line count in StringUtil

--- Classes/Strings/StringUtil.java
+++ Classes/Strings/StringUtil.java
@@ -1,5 +1,4 @@
 class StringUtil{
-    private static int min(int a, int b){ return a<b ? a : b; }
     // pi[i] = max( 0<=k<=i ){ k: s[0...k-1] = s[i-(k-1)...i] }
     public final static int[] kmp(char[] s){
         final int[] pi = new int[s.length];
@@ -12,17 +11,7 @@ class StringUtil{
         }
         return pi;
     }
-    public final static int[] kmp(String s){
-        final int[] pi = new int[s.length()];
-        int j;
-        for(int i = 1; i < s.length(); ++i){
-            j = pi[i-1];
-            while(j > 0 && s.charAt(i) != s.charAt(j)) j = pi[j - 1];
-            if(s.charAt(i) == s.charAt(j)) ++j;
-            pi[i] = j;
-        }
-        return pi;
-    }
+    public final static int[] kmp(String s){ return kmp(s.toCharArray()); }
     // z[i] = max( 0<=k<=n-i ){ k: s[0...k-1] = s[i...i+k-1] } (except z[0] = 0)
     public final static int[] z(char[] s){
         final int[] z = new int[s.length];
@@ -37,19 +26,7 @@ class StringUtil{
         }
         return z;
     }   
-    public final static int[] z(String s){
-        final int[] z = new int[s.length()];
-        int l = 0, r = 0;
-        for(int i = 1; i < s.length(); ++i){
-            if(i < r) z[i] = min(r - i, z[i - l]);
-            while(i + z[i] < s.length() && s.charAt(z[i]) == s.charAt(i + z[i])) ++z[i];
-            if(i + z[i] > r){
-                l = i;
-                r = i + z[i];
-            }
-        }
-        return z;
-    }  
+    public final static int[] z(String s){ return z(s.toCharArray()); }  
     public final static boolean isSubsequence(String a, String b){
         int ap = 0, bp = 0;
         while (ap < a.length() && bp < b.length()) {
@@ -82,4 +59,5 @@ class StringUtil{
             }
         return res;
     }
+    private static int min(int a, int b){ return a<b ? a : b; }
 }
