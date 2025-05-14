ff9bb315087bf551940e1361f4f474b2c4e42772
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-05-14 17:19:05 +0300

    added maxEnding to Manacher

--- Strings/Manacher.java
+++ Strings/Manacher.java
@@ -20,7 +20,9 @@ class Manacher{
     }
     public Manacher(String t) { this(t.toCharArray()); }
     // returns whether t[l...r] is a palindrome (O(1))
-    public final boolean isPalindrome(int l, int r) { return d[l + r + 2] > r - l; }
+    public final boolean isPalindrome(int l, int r) {
+        return d[l + r + 2] > r - l;
+    }
     // returns [l, r] that is the longest palindromic substring of t (O(n))
     public final int[] maxPalindrome() {
         int center = 0;
@@ -34,6 +36,19 @@ class Manacher{
     public final int maxOddAt(int i) { return d[(i + 1) << 1] - 1; }
     // returns length of longest palindromic substring centered at i,i+1 (O(1))
     public final int maxEvenAt(int i) { return d[3 + (i << 1)] - 1; }
-    private final static int min(int x, int y){ return x < y ? x : y; }
-    private final static int max(int x, int y){ return x > y ? x : y; }
+    // returns array where array[i] is length of longest palindromic substring ending at i (O(n))
+    public final int[] maxEnding() {
+        int n = (d.length - 3) >> 1, last = 0;
+        int[] res = new int[n];
+        for(int i = 0; i < n; ++i)
+            for(int j = max(last, i); j < min(n, i + (d[(i + 1) << 1] >> 1)); last = ++j)
+                res[j] = 1 + ((j - i) << 1); 
+        last = 0;
+        for(int i = 1; i < n; ++i)
+            for(int j = max(last, i); j < min(n, i + (d[1 + (i << 1)] >> 1)); last = ++j)
+                res[j] = max(res[j], (j - i + 1) << 1);
+        return res;
+    }
+    private final static int min(int x, int y) { return x < y ? x : y; }
+    private final static int max(int x, int y) { return x > y ? x : y; }
 }
