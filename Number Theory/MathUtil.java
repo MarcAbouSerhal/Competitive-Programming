e94bdfea7634a9c16a4c17fe2303879d489d7eac
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-05-16 14:27:31 +0300

    added divisors to MathUtil

--- Number Theory/MathUtil.java
+++ Number Theory/MathUtil.java
@@ -8,4 +8,17 @@ class MathUtil{
                 div[j].add(i);
         return div;
     }
+    // finds divisors of a (O(sqrt(a)))
+    public final  static ArrayList<Integer> divisors(int a){
+        ArrayList<Integer> factors = new ArrayList<>();
+        factors.add(1);
+        int i = 2;
+        while(i * i<=a){
+            if(a % i == 0) factors.add(i);
+            i++;
+        }
+        for(int j = factors.get(factors.size() - 1) * factors.get(factors.size() - 1) == a ? factors.size() - 2 : factors.size() - 1; j>=0; --j)
+            factors.add(a/factors.get(j));
+        return factors;
+    }
 }
