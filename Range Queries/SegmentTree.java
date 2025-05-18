5612b1b72078d472ee8ca4402c2dc6f443d18dbf
MarcAbouSerhal <marcabouserhal@gmail.com>
2025-05-18 20:25:11 +0300

    Segtree is now iterative, removed boundaryseggy

--- Range Queries/SegmentTree.java
+++ Range Queries/SegmentTree.java
@@ -1,31 +1,29 @@
 // replace X with type of property (or Tuple of properties)
-// if X is primitive: replace null by value that will be ignored by op
 class SegmentTree{
     private final X[] tree;
-    private int leaves;
-    // (both O(n.T(op)))
-    public SegmentTree(int n, X v){
-        leaves = n;
-        if((leaves & (leaves - 1)) != 0)
-            leaves = Integer.highestOneBit(leaves) << 1;
-        tree = new X[(leaves << 1) - 1];
-        for(int i = 0; i < n; ++i) tree[i + leaves - 1] = v;
-        for(int i = leaves - 2; i >= 0; --i) tree[i] = op(tree[(i << 1) + 1], tree[(i + 1) << 1]);
-    }
-    public SegmentTree(X[] a){
-        leaves = a.length;
-        if((leaves & (leaves - 1)) != 0)
-            leaves = Integer.highestOneBit(leaves) << 1;
+    private final int leaves;
+    // (O(n.T(op)))
+    public SegmentTree(X[] a) {
+        leaves = a.length <= 1 ? 1 : 1 << (32 - Integer.numberOfLeadingZeros(a.length - 1)); 
         tree = new X[(leaves << 1) - 1];
         for(int i = 0; i < a.length; ++i) tree[i + leaves - 1] = a[i];
         for(int i = leaves - 2; i >= 0; --i) tree[i] = op(tree[(i << 1) + 1], tree[(i + 1) << 1]);
     }
     // returns f(a[l...r]) (O(log(n)*T(op)))
-    public final X get(int l, int r){
-        return get(l, r, 0, 0, leaves - 1);
+    public final X get(int l, int r) {
+        X lOp = id, rOp = id;
+        l += leaves - 1;
+        r += leaves;
+        while(l < r) {
+            if((l & 1) == 0) lOp = op(lOp, tree[l++]);
+            if((r & 1) == 0) rOp = op(tree[--r], rOp);
+            l = (l - 1) >> 1;
+            r = (r - 1) >> 1;
+        }
+        return op(lOp, rOp);
     }
     // sets a[x] to v (O(log(n)*T(op)))
-    public final void set(int x, X v){
+    public final void set(int x, int v) {
         x += leaves - 1;
         tree[x] = v;
         while(x != 0){
@@ -33,28 +31,17 @@ class SegmentTree{
             tree[x] = op(tree[(x << 1) + 1], tree[(x + 1) << 1]);
         }
     }
-    private final X get(int l, int r, int x, int lx, int rx){
-        if(rx < l || lx > r) return null;
-        if(lx >= l && rx <= r) return tree[x];
-        int mid = (lx + rx) >> 1;
-        return op(get(l, r, (x << 1) + 1, lx, mid), get(l, r, (x + 1) << 1, mid + 1, rx));
-    }
-    // CHANGE THESE FUNCTIONS
-    private static final X op(X a, X b){
-        if(a == null) return b;
-        if(b == null) return a;
+    // CHANGE THESE 
+    private static final X id;
+    private static final X op(X a, X b) {
         // define associative operation here (f(f(a,b),c)=f(a,f(b,c)))
     }
 
     // WALKING ON SEGTREE (optional)
     // // returns smallest i in [l...r] such that prop(a[i]), -1 if there is no such element (O(log(n)))
-    // public final int first(int l, int r){
-    //     return first(l, r, 0, 0, leaves - 1);
-    // }
+    // public final int first(int l, int r){ return first(l, r, 0, 0, leaves - 1); }
     // // returns biggest i in [l...r] such that prop(a[i]), -1 if there is no such element (O(log(n)))
-    // public final int last(int l, int r){
-    //     return last(l, r, 0, 0, leaves - 1);
-    // }
+    // public final int last(int l, int r){ return last(l, r, 0, 0, leaves - 1); }
     // // fix this pseudocode
     // private final int first(int l, int r, int x, int lx, int rx){
     //     if(rx < l || lx > r) return -1;
