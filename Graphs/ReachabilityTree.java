7a78c219ed0872dbb2a61b698b8f9a8467e8822c
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-05-25 15:11:24 +0300

    fixed firstEdgeConnecting in ReachabilityTree

--- Graphs/ReachabilityTree.java
+++ Graphs/ReachabilityTree.java
@@ -16,10 +16,8 @@ class ReachabilityTree{
         outEdge = new int[m];
         this.n = n;
         this.m = m;
-        for(int i = 0; i < m; ++i)
-            outVertex[i] = outEdge[i] = -1;
-        for(int i = 0; i < n + m; ++i) 
-            dsu[i] = up[0][i] = -1;
+        for(int i = 0; i < m; ++i) outVertex[i] = outEdge[i] = -1;
+        for(int i = 0; i < n + m; ++i) dsu[i] = up[0][i] = -1;
     }
     // adds edge (u,v) (O(log(m + n)))
     public final void addEdge(int u, int v) {
@@ -49,23 +47,21 @@ class ReachabilityTree{
     // or -1 if there is none (O(log(n + m)))
     public final int firstEdgeConnecting(int u, int v) {
         if(find(u) != find(v)) return -1;
+        int ind = vertexIndex[v];
         for(int i = log - 1; i >= 0; --i) {
-            int p1 = up[i][u], p2 = up[i][v];
-            if(p1 != -1 && p1 == p2) {
-                u = p1;
-                v = p2;
-            }
+            int e = up[i][u];
+            if(e != -1 && (inVertex[e - n] > ind || ind > outVertex[e - n])) u = e;
         }
         return up[0][u] - n;
     }
     // returns last edge added to component containing u (O(log(n + m)))
-    public final int lastEdge(int u) { return find(u) - n; }
+    public final int lastEdge(int u) { int w = find(u); return w == u ? -1 : w - n; }
     // returns range [l,r] of vertices in the permutation
     // that are connected to u up to edge e (O(log(n + m)))
     public final int[] vertexRange(int u, int e) {
         e = lastEdgeBefore(u, e);
         if(e == -1) return new int[] {vertexIndex[u], vertexIndex[u]};
-        return new int[] {inVertex[e], outVertex[e]};
+        else return new int[] {inVertex[e], outVertex[e]};
     }
     // returns range [l,r] of edges in the permutation
     // that are connected to u up to edge e 
@@ -73,7 +69,7 @@ class ReachabilityTree{
     public final int[] edgeRange(int u, int e) {
         e = lastEdgeBefore(u, e);
         if(e == -1) return null;
-        return new int[] {inEdge[e], outEdge[e]};
+        else return new int[] {inEdge[e], outEdge[e]};
     }
     // returns permutation of vertices
     public final int[] getVertexPermutation() { return vertexPermutation; }
@@ -85,8 +81,7 @@ class ReachabilityTree{
     public final int[] getEdgeIndex() { return inEdge; }
     // private methods
     private final int find(int a) {
-        while(dsu[a] != -1 && dsu[dsu[a]] != -1)
-            a = dsu[a] = dsu[dsu[a]];
+        while(dsu[a] != -1 && dsu[dsu[a]] != -1) a = dsu[a] = dsu[dsu[a]];
         return dsu[a] < 0 ? a : dsu[a];
     }
     // builds permutations and sets up queries (O((n + m)log(n + m)))
@@ -95,14 +90,10 @@ class ReachabilityTree{
             for(int i = 0; i < n + m; ++i)
                 up[j][i] = up[j - 1][i] == -1 ? -1 : up[j - 1][up[j - 1][i]];
         edgeTick = 0;
-        for(int i = 0; i < n + m; ++i)
-            if(up[0][i] == -1)
-                dfs(i);
+        for(int i = 0; i < n + m; ++i) if(up[0][i] == -1) dfs(i);
     }
     private final void dfs(int u) {
-        if(u < n) {
-            vertexPermutation[vertexIndex[u] = vertexTick++] = u;  
-        }
+        if(u < n) vertexPermutation[vertexIndex[u] = vertexTick++] = u;  
         else {
             int e = u - n;
             inVertex[e] = vertexTick;
