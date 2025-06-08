87ac14961000973976432e2169de812e64cf65bc
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-06-08 20:58:21 +0300

    added XOR, implies, forcing and AMO to TwoSat

--- Graphs/TwoSat.java
+++ Graphs/TwoSat.java
@@ -1,27 +1,74 @@
 class TwoSat{
     private final ArrayList<Integer>[] adj;
-    private final int n;
-    public TwoSat(int n_){
-        n = n_;
+    private final int n, vars;
+    private int aux;
+    public TwoSat(int n_) {
+        n = vars = n_;
         n_ <<= 1;
         adj = new ArrayList[n_];
         for(int i = 0; i < n_; ++i) adj[i] = new ArrayList<>();
     }
-    // adds the clause (u has u_value OR v has v_value) (O(1))
-    public final void addClause(int u, boolean u_value, int v, boolean v_value){
-        adj[(u << 1) + (u_value ? 0 : 1)].add((v << 1) + (v_value ? 1 : 0));
-        adj[(v << 1) + (v_value ? 0 : 1)].add((u << 1) + (u_value ? 1 : 0));
+    // aux_ is number of auxiliary vertices needed 
+    // (each AMO of k >= 3 variables uses k auxiliary variables)
+    public TwoSat(int n_, int aux_) {
+        aux = vars = n_;
+        n = n_ + aux_;
+        n_ = n << 1;
+        adj = new ArrayList[n_];
+        for(int i = 0; i < n_; ++i) adj[i] = new ArrayList<>();
+    }
+    // adds the clause (u -> v) (O(1))
+    public final void implies(int u, int v) { adj[u].add(v); adj[NOT(v)].add(NOT(u)); }
+    // adds the clause (u OR v) (O(1))
+    public final void OR(int u, int v){ adj[NOT(u)].add(v); adj[NOT(v)].add(u); }
+    // adds the clause (u XOR v) (O(1))
+    public final void XOR(int u, int v) { adj[NOT(u)].add(v); adj[NOT(v)].add(u); adj[u].add(NOT(v)); adj[v].add(NOT(u)); }
+    // adds the clause (u) (O(1))
+    public final void force(int u) { adj[NOT(u)].add(u); }
+    // removes the clause (u) (O(1)) (assumes last thing added to adj[NOT(u)] was u)
+    public final void remove(int u) { adj[NOT(u)].remove(adj[NOT(u)].size() - 1); }
+    // adds the clause (at most one of u[0], u[1], ..., u[k - 1] is true) (O(k))
+    public final void atMostOneTrue(ArrayList<Integer> u) {
+        if(u.size() <= 1) return;
+        if(u.size() == 2) OR(NOT(u.get(0)), NOT(u.get(1)));
+        else {
+            int k = u.size();
+            OR(NOT(u.get(0)), aux);
+            for(int i = 1; i < k; ++i) {
+                OR(NOT(u.get(i)), aux + 1);
+                OR(NOT(u.get(i)), NOT(aux));
+                OR(NOT(aux), ++aux);
+            }
+            ++aux;
+        }
+    }
+    // adds the clause (at most one of u[0], u[1], ..., u[k - 1] is false) (O(k))
+    public final void atMostOneFalse(ArrayList<Integer> u) {
+        if(u.size() <= 1) return;
+        if(u.size() == 2) OR(u.get(0), u.get(1));
+        else {
+            int k = u.size();
+            OR(u.get(0), NOT(aux));
+            for(int i = 1; i < k; ++i) {
+                OR(u.get(i), NOT(aux + 1));
+                OR(u.get(i), aux);
+                OR(aux, NOT(++aux));
+            }
+            ++aux;
+        }  
     }
     // returns valid configuration if there exists one, null if there isn't (O(n + m))
     public final boolean[] answer(){
-        final int[] id = getSCCs();
-        final boolean[] ans = new boolean[n];
-        for(int i = 0; i < n; ++i){
-            if(id[i << 1] == id[(i << 1) + 1]) return null;
-            ans[i] = id[i << 1] > id[(i << 1) + 1];
+        int[] id = getSCCs();
+        boolean[] ans = new boolean[vars];
+        for(int u = 0; u < vars; ++u){
+            if(id[u] == id[u + n]) return null;
+            ans[u] = id[u] > id[u + n];
         }
         return ans;
     }
+    // returns vertex corresponding to complement of u (O(1))
+    public final int NOT(int u) { return u < n ? u + n : u - n; }
     private static int tick;
     private static int[] t;
     private static int[] id;
@@ -34,8 +81,7 @@ class TwoSat{
         t = new int[n];
         s = new Stack(n);
         for(int i=0; i < n; ++i)
-            if(t[i] == 0)
-                dfs(i);
+            if(t[i] == 0) dfs(i);
         return id;
     }
     private final int dfs(int u){
@@ -58,5 +104,5 @@ class TwoSat{
         public final void add(int u){ s[size++] = u; }
         public final int pop(){ return s[--size]; }
     }
-    private static final int min(int a, int b){ return a<b ? a : b; }
+    private static final int min(int a, int b){ return a < b ? a : b; }
 }
