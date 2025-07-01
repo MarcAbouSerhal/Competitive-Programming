3dbfcb813f8bad81cab0f880b2acc282705a3406
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-07-01 21:26:21 +0300

    HLD now flattens all chain into 1 array which is also an Euler tour

--- Trees/HeavyLightDecomposition.java
+++ Trees/HeavyLightDecomposition.java
@@ -1,97 +1,74 @@
 class HeavyLightDecomposition{
-    private final int[] p, d, idx, root;
-    private final DS[] ds;
+    private final int[] p, d, in, out, root;
+    private final DS ds;
     private final int edgeProp;
-
     private static ArrayList<Integer>[] adj;
-    private static X[] a, chain;
+    private static X[] a, b;
     private static int[] heavyChild;
-    private static int chainSize = 0;
+    private static int tick = 0;
     // set edgeProp to true if values are on edges: a[u] = value on (u, parent[u]) and a[src] is ignored
-    public HeavyLightDecomposition(ArrayList<Integer>[] adj, X[] a) { this(adj, a, 0, false); }
-    public HeavyLightDecomposition(ArrayList<Integer>[] adj, X[] a, boolean edgeProp) { this(adj, a, 0, edgeProp); }
-    public HeavyLightDecomposition(ArrayList<Integer>[] adj, X[] a, int src) { this(adj, a, src, false); }
     public HeavyLightDecomposition(ArrayList<Integer>[] adj, X[] a, int src, boolean edgeProp) {
         int n = adj.length;
         p = new int[n];
         d = new int[n];
-        idx = new int[n];
+        in = new int[n];
+        out = new int[n];
         root = new int[n];
-        ds = new DS[n];
         this.edgeProp = edgeProp ? 1 : 0;
         HeavyLightDecomposition.adj = adj;
         HeavyLightDecomposition.a = a;
-        chain = new X[n];
-        heavyChild = new int[n];
-
+        HeavyLightDecomposition.b = new X[n];
+        HeavyLightDecomposition.heavyChild = new int[n];
+        HeavyLightDecomposition.tick = 0;
         dfs1(src, -1);
         dfs2(src, src);
-        chain = null;
-        heavyChild = null; // trash unused arrays
+        ds = new DS(b);
+        HeavyLightDecomposition.b = null;
+        HeavyLightDecomposition.heavyChild = null;
     }
     // sets value[u] or value[u, p(u)] to x (O(T(DS)))
-    public final void set(int u, X x) { ds[root[u]].set(idx[u], x); }
+    public final void set(int u, X x) { ds.set(in[u], x); }
     // returns property of the path from u to v (O(log(n)T(DS)))
-    public final X get(int u, int v) {
-        X ans = null;
+    public final X getPathProperty(int u, int v) {
+        X ans = id;
         while(root[u] != root[v]) {
             if(d[root[u]] < d[root[v]]) { u ^= v; v ^= u; u ^= v; }
-            ans = op(ans, ds[root[u]].get(0, idx[u]));
+            ans = op(ans, ds.get(in[root[u]], in[u]));
             u = p[root[u]];
         }
         if(u == v && edgeProp == 1) return ans;
-        return op(ans, ds[root[u]].get(min(idx[u], idx[v]) + edgeProp, max(idx[u], idx[v])));
+        return op(ans, ds.get(min(in[u], in[v]) + edgeProp, max(in[u], in[v])));
     }
-    // CHANGE THESE FUNCTIONS
+    // returns property of subtree of u (O(T(DS)))
+    public final X getSubtreeProperty(int u) { return ds.get(in[u] + edgeProp, out[u]); }
+    // CHANGE THESE
+    private static final X id;
     private static final X op(X a, X b){
         // define associative operation here (f(f(a,b),c)=f(a,f(b,c)))
     }
-
-    // applies update on path from u to v (O(log(n)T(DS)))
-    // same as get(u, v) but with update(l, r, x) instead of get(l, r)
-    
-    // applies update on subtree of u (O(log(n) + T(DS)))
-    // lazy: ds with range updates
-    // find euler tour of tree
-    // to update subtree(u):
-    //   lazy.update(in[u], out[u], x)
-    //   if u != root[u]: ds[root[u]].update(idx[u], end, x)
-    // before querying ds[root[u]]:
-    //   ds[root[u]].update(all, lazy[root[u]]) 
-    //   reset it lazy[root[u]]
-
-    // private methods
     private final int dfs1(int u, int par) {
         p[u] = par;
         int size = 1, heavySize = 0;
-        for(int v: adj[u]) 
-            if(v != par) {
-                d[v] = d[u] + 1;
-                int childSize = dfs1(v, u);
-                size += childSize;
-                if(childSize > heavySize) {
-                    heavySize = childSize;
-                    heavyChild[u] = v;
-                }
+        for(int v: adj[u]) {
+            if(v == par) continue;
+            d[v] = d[u] + 1;
+            int childSize = dfs1(v, u);
+            size += childSize;
+            if(childSize > heavySize) {
+                heavySize = childSize;
+                heavyChild[u] = v;
             }
+        }
         return size;
     }
     private final void dfs2(int u, int rt) {
-        idx[u] = chainSize;
-        root[u] = rt;
-        chain[chainSize++] = a[u];
-        if(adj[u].isEmpty() || (adj[u].size() == 1 && p[u] != -1)) {
-            X[] resizedChain = new X[chainSize];
-            for(int i = 0; i < chainSize; ++i) resizedChain[i] = chain[i];
-            ds[rt] = new DS(resizedChain);
-            chainSize = 0;
-        }
-        else {
+        b[in[u] = tick++] = a[u];
+        if(adj[u].size() != 0 && (adj[u].size() != 1 || p[u] == -1)) {
             dfs2(heavyChild[u], rt);
             for(int v: adj[u])
-                if(v != p[u] && v != heavyChild[u])
-                    dfs2(v, v);
+                if(v != p[u] && v != heavyChild[u]) dfs2(v, v);
         }
+        out[u] = tick - 1;
     }
     private static final int min(int x, int y) { return x < y ? x : y; }
     private static final int max(int x, int y) { return x > y ? x : y; }
