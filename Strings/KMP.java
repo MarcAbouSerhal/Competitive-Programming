4791f88c9c310cb603768178806c2e6778e68db9
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-07-15 19:53:28 +0300

    fixed KMP

--- Strings/KMP.java
+++ Strings/KMP.java
@@ -1,6 +1,6 @@
 class KMP {
     // returns pi where pi[i] = max( 0<=k<=i ){ k: s[0...k-1] = s[i-(k-1)...i] } (O(n))
-    public final static int[] kmp(char[] s){
+    public final static int[] pi(char[] s){
         int[] pi = new int[s.length];
         int j;
         for(int i = 1; i < s.length; ++i){
@@ -10,16 +10,16 @@ class KMP {
         }
         return pi;
     }
-    // returns sufCount where sufCount[i] = #{j: s[j...j + n - 1 - i] = s[i...n - 1]} (O(n))
-    public final static int[] sufCount(int[] pi) {
+    // returns prefCount where prefCount[i] = #{j: s[j...j + i] = s[0...i]} (O(n))
+    public final static int[] prefCount(int[] pi) {
         int n = pi.length;
-        int[] sufCount = new int[n];
-        for(int i: pi) if(i > 0) ++sufCount[n - i];
-        for(int i = n - 1; i > 0; ++i) {
-            if(pi[i - 1] > 0) sufCount[n - pi[i - 1]] += sufCount[n - i];
-            ++sufCount[n - i - 1];
+        int[] prefCount = new int[n];
+        for(int i: pi) if(i > 0) ++prefCount[i - 1];
+        for(int i = n - 1; i > 0; --i) {
+            if(pi[i - 1] > 0) prefCount[pi[i - 1] - 1] += prefCount[i - 1];
+            ++prefCount[i];
         }
-        ++sufCount[n - 1];
-        return sufCount;
+        ++prefCount[0];
+        return prefCount;
     }
 }
