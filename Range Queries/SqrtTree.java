5bb16125dd792908514959abb4a5aed802b0315d
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-07-23 20:45:52 +0300

    Memort optimization to SqrtTree

--- Range Queries/SqrtTree.java
+++ Range Queries/SqrtTree.java
@@ -2,7 +2,7 @@
 class SqrtTree{
     private final int n, lg;
     private final X[] a;
-    private final int[] floorLog, onLayer;
+    private final int[] onLayer;
     private final ArrayList<Integer> layers = new ArrayList<>();
     private final X[][] pref, suf, between;
     // (O(nloglog(n)))
@@ -10,8 +10,6 @@ class SqrtTree{
         a = arr;
         n = a.length;
         lg = 32 - Integer.numberOfLeadingZeros(n);
-        floorLog = new int[1 << lg];
-        for(int i = 1; i < floorLog.length; ++i) floorLog[i] = 1 + floorLog[i >> 1];
         onLayer = new int[lg + 1];
         int tlg = lg;
         while(tlg > 1) {
@@ -19,7 +17,7 @@ class SqrtTree{
             layers.add(tlg);
             tlg = (tlg + 1) >> 1;
         }
-        for(int i = lg - 1; i >= 0; --i) onLayer[i] = max(onLayer[i], onLayer[i + 1]);
+        for(int i = lg - 1; i >= 0; --i) onLayer[i] = Math.max(onLayer[i], onLayer[i + 1]);
         pref = new X[layers.size()][n];
         suf = new X[layers.size()][n];
         between = new X[layers.size()][1 << lg];
@@ -29,7 +27,7 @@ class SqrtTree{
     public final X get(int l, int r) {
         if(l == r) return a[l];
         else if(l + 1 == r) return op(a[l], a[r]);
-        int layer = onLayer[floorLog[l ^ r]], layerSz = layers.get(layer);
+        int layer = onLayer[31 - Integer.numberOfLeadingZeros(l ^ r)], layerSz = layers.get(layer);
         int bSzLog = (layerSz + 1) >> 1;
         int bCntLog = layerSz >> 1;
         int lBound = (l >> layerSz) << layerSz;
@@ -48,7 +46,7 @@ class SqrtTree{
         int bCnt = 0;
         for (int l = lBound; l < rBound; l += bSz) {
             bCnt++;
-            int r = min(l + bSz, rBound);
+            int r = Math.min(l + bSz, rBound);
             pref[layer][l] = a[l];
             for (int i = l + 1; i < r; ++i) pref[layer][i] = op(pref[layer][i - 1], a[i]);
             suf[layer][r - 1] = a[r - 1];
@@ -61,8 +59,6 @@ class SqrtTree{
             for(int j = i + 1; j < bCnt; ++j) between[layer][shift + j] = op(between[layer][shift + j - 1], suf[layer][lBound + (j << bSzLog)]);
         }
     }
-    private static final int max(int a, int b) { return a > b ? a : b; }
-    private static final int min(int a, int b) { return a < b ? a : b; }
     // CHANGE THESE FUNCTIONS
     private static final X op(X a, X b) {
         if(a == null) return b;
