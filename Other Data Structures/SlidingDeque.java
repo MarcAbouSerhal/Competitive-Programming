2f1f8b22127cb836642f11ca31ba652636f6b0c4
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-07-24 22:00:03 +0300

    fixed SlidingDeque

--- Other Data Structures/SlidingDeque.java
+++ Other Data Structures/SlidingDeque.java
@@ -1,13 +1,11 @@
 // Use: SlidingDeque<Integer> maxdq = new SlidingDeque<>(k, (x, y) -> x > y ? x : y);
 // use this for sliding window, queries [l,r] where both l and r are non-decreasing
 class SlidingDeque<E>{
-    @FunctionalInterface
-    private static interface Operation<F> { F apply(F a, F b); }
     private final Operation<E> operation;
     private int n1 = 0, n2 = 0;
     private final Object[] s1, s2, opS1, opS2;
     // k is max size of window, op is lambda function
-    public SlidingDeque(int k, Operation<E> op){ 
+    public SlidingDeque(int k, Operation<E> op) { 
         s1 = new Object[k];
         s2 = new Object[k];
         opS1 = new Object[k];
@@ -15,12 +13,8 @@ class SlidingDeque<E>{
         operation = op;
     }
     // clears the dq (O(1))
-    public final void clear(){
-        n1 = n2 = 0;
-    }
-    public final int size(){ 
-        return n1 + n2;
-    }
+    public final void clear() { n1 = n2 = 0; }
+    public final int size() {  return n1 + n2; }
     // adds to the back of the dq (O(1))
     public final void addBack(E e){
         s1[n1] = e;
@@ -35,18 +29,18 @@ class SlidingDeque<E>{
     }
     // removes from the back of the dq and returns it (O(1) amortized)
     public final E removeBack(){
-        if(n1 == 0)
-            for(--n2; n2 >= 0; --n2)
-                addBack((E)s2[n2]);
-        ++n2;
+        if(n1 == 0) {
+            for(--n2; n2 >= 0; --n2) addBack((E)s2[n2]);
+            n2 = 0;
+        }
         return (E)s1[--n1];
     }
     // removes from the front of the dq and returns it (O(1) amortized)
     public final E removeFront(){
-        if(n2 == 0)
-            for(--n1; n1 >= 0; --n1)
-                addFront((E)s1[n1]);
-        ++n1;
+        if(n2 == 0) {
+            for(--n1; n1 >= 0; --n1) addFront((E)s1[n1]);
+            n1 = 0;
+        }
         return (E)s2[--n2];
     }
     // returns op(elements of the dq) (O(T(op)))
@@ -55,4 +49,6 @@ class SlidingDeque<E>{
         if(n2 == 0) return (E)opS1[n1 - 1];
         return operation.apply((E)opS1[n1 - 1], (E)opS2[n2 - 1]);
     }
+    @FunctionalInterface
+    static interface Operation<F> { F apply(F a, F b); }
 }
