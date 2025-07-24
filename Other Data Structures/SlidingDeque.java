57c007cb2dac8b56770529707db93ae356ad48e2
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-07-24 22:14:58 +0300

    fixed time SlidingDeque

--- Other Data Structures/SlidingDeque.java
+++ Other Data Structures/SlidingDeque.java
@@ -1,54 +1,50 @@
-// Use: SlidingDeque<Integer> maxdq = new SlidingDeque<>(k, (x, y) -> x > y ? x : y);
 // use this for sliding window, queries [l,r] where both l and r are non-decreasing
-class SlidingDeque<E>{
-    private final Operation<E> operation;
+class SlidingDeque { 
     private int n1 = 0, n2 = 0;
-    private final Object[] s1, s2, opS1, opS2;
+    private final X[] s1, s2, opS1, opS2;
     // k is max size of window, op is lambda function
-    public SlidingDeque(int k, Operation<E> op) { 
-        s1 = new Object[k];
-        s2 = new Object[k];
-        opS1 = new Object[k];
-        opS2 = new Object[k];
-        operation = op;
+    public SlidingDeque(int k) { 
+        s1 = new int[k];
+        s2 = new int[k];
+        opS1 = new int[k];
+        opS2 = new int[k];
     }
     // clears the dq (O(1))
     public final void clear() { n1 = n2 = 0; }
     public final int size() {  return n1 + n2; }
     // adds to the back of the dq (O(1))
-    public final void addBack(E e){
+    public final void addBack(int e){
         s1[n1] = e;
         if(n1 == 0) opS1[n1++] = e;
-        else opS1[n1] = operation.apply((E)opS1[n1++ - 1], e);
+        else opS1[n1] = op(opS1[n1++ - 1], e);
     }
     // adds to the front of the dq (O(1))
-    public final void addFront(E e){
+    public final void addFront(int e){
         s2[n2] = e;
         if(n2 == 0) opS2[n2++] = e;
-        else opS2[n2] = operation.apply((E)opS2[n2++ - 1], e);
+        else opS2[n2] =op(opS2[n2++ - 1], e);
     }
     // removes from the back of the dq and returns it (O(1) amortized)
-    public final E removeBack(){
+    public final int removeBack(){
         if(n1 == 0) {
-            for(--n2; n2 >= 0; --n2) addBack((E)s2[n2]);
+            for(--n2; n2 >= 0; --n2) addBack(s2[n2]);
             n2 = 0;
         }
-        return (E)s1[--n1];
+        return s1[--n1];
     }
     // removes from the front of the dq and returns it (O(1) amortized)
-    public final E removeFront(){
+    public final int removeFront(){
         if(n2 == 0) {
-            for(--n1; n1 >= 0; --n1) addFront((E)s1[n1]);
+            for(--n1; n1 >= 0; --n1) addFront(s1[n1]);
             n1 = 0;
         }
-        return (E)s2[--n2];
+        return s2[--n2];
     }
     // returns op(elements of the dq) (O(T(op)))
-    public final E getOp(){
-        if(n1 == 0) return (E)opS2[n2 - 1];
-        if(n2 == 0) return (E)opS1[n1 - 1];
-        return operation.apply((E)opS1[n1 - 1], (E)opS2[n2 - 1]);
+    public final int getOp(){
+        if(n1 == 0) return opS2[n2 - 1];
+        if(n2 == 0) return opS1[n1 - 1];
+        return op(opS1[n1 - 1], opS2[n2 - 1]);
     }
-    @FunctionalInterface
-    static interface Operation<F> { F apply(F a, F b); }
+    private static final X op(X a, X b) { /* define associative operation here (f(a, f(b, c)) = f(f(a, b), c)) */ }
 }
