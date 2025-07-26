90a3696efcf98b20d96d08b5f8c05d89648d9d13
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-07-26 10:15:40 +0300

    optimized LinearSieve

--- Number Theory/LinearSieve.java
+++ Number Theory/LinearSieve.java
@@ -1,66 +1,55 @@
 // use this for divisors of numbers <= 1e7
 class LinearSieve{
-    static final class Pair{
-        int x,y;
-        public Pair(int x, int y){ this.x=x; this.y=y; }
-    }
-    final int[] sp; // sp[i] = smallest prime divisor of i
-    final int[] pf; // pf[i] = number of unique prime factors of i
+    final int[] gp; // sp[i] = smallest prime divisor of i
     // (O(c))
     public LinearSieve(int c){
-        sp = new int[c+1];
-        pf = new int[c+1];
+        gp = new int[c + 1];
         for(int i = 2; i <= c; ++i)
-            if(sp[i] == 0)
-                for(int j = 1; i * j <= c; ++j)
-                    if(sp[i * j] == 0) 
-                        pf[i * j] = sp[j] == (sp[i * j] = i) ? pf[j] : pf[j] + 1;
+            if(gp[i] == 0)
+                for(int j = i; j <= c; j += i) gp[j] = i;
     }
     // (O(log(x)/log(log(x))))
-    public final ArrayList<Pair> primeFactors(int x){
-        ArrayList<Pair> res = new ArrayList<>();
-        while(x > 1){
-            int p = sp[x], cnt = 0;
-            while(sp[x] == p){
-                x /= p;
-                ++cnt;
-            }
-            res.add(new Pair(p, cnt));
+    public final ArrayList<PrimeFactor> primeFactors(int x){
+        ArrayList<PrimeFactor> res = new ArrayList<>();
+        while(x != 1){
+            int p = gp[x], e = 1;
+            while(gp[x = x / p] == p) ++e;
+            res.add(new PrimeFactor(p, e));
         }
         return res;
     }
     // (O(2 ^ pf[x])
     public final int[] uniquePrimeProductDivisors(int x){
         ArrayList<Integer> primes = new ArrayList<>();
-        while(x > 1){
-            int p = sp[x];
-            while(sp[x] == p)
-                x /= p;
+        while(x != 1){
+            int p = gp[x];
+            while(gp[x = x / p] == p);
             primes.add(p);
         }
         int[] res = new int[1 << primes.size()];
-        for(int i = 0; i< 1 << primes.size(); ++i){
-            res[i] = 1;
-            for(int j = 0; j < primes.size(); ++j)
-                if((i & (1 << j)) > 0) res[i] *= primes.get(j);
-        }
+        res[0] = 1;
+        for(int i = 0; i < primes.size(); ++i) res[1 << i] = primes.get(i);
+        for(int i = 1; i < 1 << primes.size(); ++i) res[i] = res[i & -i] * res[i & (i - 1)];
         return res;
     }
     // (O(d(x)))
     public final ArrayList<Integer> divisors(int x){
-        ArrayList<Pair> pfs = primeFactors(x);
         ArrayList<Integer> ans = new ArrayList<>();
-        f(1, 0, pfs, ans);
+        f(1, 0, primeFactors(x),  ans);
         ans.add(1);
         return ans;
     }
-    private static final void f(int x, int i, ArrayList<Pair> pfs,ArrayList<Integer> ans){
+    private static final void f(int x, int i, ArrayList<PrimeFactor> pfs,ArrayList<Integer> ans){
         if(i >= pfs.size()) return;
         f(x, i + 1, pfs, ans);
-        for(int cnt = 1; cnt <= pfs.get(i).y; ++cnt){
-            x *= pfs.get(i).x;
+        for(int cnt = 1; cnt <= pfs.get(i).e; ++cnt){
+            x *= pfs.get(i).p;
             ans.add(x);
             f(x, i + 1, pfs, ans);
         }
     }
+}
+class PrimeFactor{
+    final int p, e;
+    public PrimeFactor(int p, int e){ this.p = p; this.e = e; }
 }
