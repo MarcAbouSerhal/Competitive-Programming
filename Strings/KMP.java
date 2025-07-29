8ea8abf21a4d781de558b1b47fab0b68ee3e2809
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-07-29 18:37:57 +0300

    added KMP automaton'

--- Strings/KMP.java
+++ Strings/KMP.java
@@ -1,5 +1,5 @@
 class KMP {
-    // returns pi where pi[i] = max( 0<=k<=i ){ k: s[0...k-1] = s[i-(k-1)...i] } (O(n))
+    // returns pi where pi[i] = max{0 <= k < i: s[0, k - 1] = s[i - (k - 1), i]} (O(n))
     public final static int[] pi(char[] s){
         int[] pi = new int[s.length];
         int j;
@@ -21,5 +21,17 @@ class KMP {
         }
         ++prefCount[0];
         return prefCount;
+    }
+    // returns transtions of KMP automaton of s (O(n.k))
+    public static final int[][] automaton(char[] s, int k, char base) {
+        int n = s.length;
+        int[] pi = pi(s);
+        int[][] aut = new int[n + 1][k];
+        for(int st = 0; st <= n; ++st)
+            for(int c = 0; c < k; ++c)
+                if(st < n && s[st] == base + c) aut[st][c] = st + 1;
+                else if (st == 0) aut[st][c] = 0;
+                else aut[st][c] = aut[pi[st - 1]][c];
+        return aut;
     }
 }
