3dbee3fe747ad5c9a308976af867cb7bd256989a
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-07-31 23:12:15 +0300

    updates to Subsequence (new functions, is now an interface)

--- Strings/Subsequence.java
+++ Strings/Subsequence.java
@@ -1,6 +1,6 @@
-class Subsequence {
-    // returns an array c[i], where LCS(s, t[l, r]) = #{l <= k <= r: c[k] <= l} (O(|s|.|t|))
-    public final static int[] characteristic(char[] s, char[] t) {
+interface Subsequence {
+    // returns an array c, where LCS(s, t[l, r]) = #{l <= k <= r: c[k] <= l} (O(|s|.|t|))
+    public static int[] characteristic(char[] s, char[] t) {
         int n = s.length, m = t.length;
         int[][] ih = new int[n + 1][m + 1], iv = new int[n + 1][m + 1];
         for(int j = 0; j <= m; ++j) ih[0][j] = j;
@@ -15,5 +15,38 @@ class Subsequence {
                     iv[i][j] = Math.min(ih[i - 1][j], iv[i][j - 1]); 
                 }
         return Arrays.copyOfRange(ih[n], 1, m + 1);
+    }
+    // returns an array c, where c[i] = min{j: t[, i] is subsequence of s[, j]} (O(|s| + |t|))
+    public static int[] smallestPrefix(char[] t, char[] s) {
+        int n = s.length, m = t.length;
+        int[] c = new int[m];
+        int p1 = 0, p2 = 0;
+        while(p1 < n && p2 < m) {
+            if(s[p1] == t[p2]) c[p2++] = p1;
+            ++p1;
+        }
+        for(; p2 < m; ++p2) c[p2] = -1;
+        return c;
+    }
+    // returns an array c, where c[i] = max{j: t[i, ] is subsequence of s[j, ]} (O(|s| + |t|))
+    public static int[] smallestSuffix(char[] t, char[] s) {
+        int n = s.length, m = t.length;
+        int[] c = new int[m];
+        int p1 = n - 1, p2 = m - 1;
+        while(p1 >= 0 && p2 >= 0) {
+            if(s[p1] == t[p2]) c[p2--] = p1;
+            --p1;
+        }
+        for(; p2 >= 0; --p2) c[p2] = -1;
+        return c;
+    }
+    public static boolean isSubsequence(char[] t, char[] s) {
+        int n = s.length, m = t.length;
+        int p1 = 0, p2 = 0;
+        while(p1 < n && p2 < m) {
+            if(s[p1] == t[p2]) ++p2;
+            ++p1;
+        }
+        return p2 == m;
     }
 }
