30580da2387aa4b7efdd238aa8ea844445eda236
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-08-02 15:36:38 +0300

    optimized SmallToLarge

--- Trees/SmallToLarge.java
+++ Trees/SmallToLarge.java
@@ -1,45 +1,48 @@
 class SmallToLarge {
     static ArrayList<Integer>[] adj;
-    static int[] size, bigChild;
-    static ArrayList<Integer>[] subtree;
+    static int[] size, bigChild, in, out, order;
+    static int tick;
     public static void start(ArrayList<Integer>[] adj, int src) {
         SmallToLarge.adj = adj;
         int n = adj.length;
         size = new int[n];
         bigChild = new int[n];
+        in = new int[n];
+        out = new int[n];
+        order = new int[n];
+        tick = 0;
         calc_size(src, -1);
         dfs(src, -1, true);
     }
     public static void calc_size(int u, int p) {
         size[u] = 1;
         bigChild[u] = -1;
-        subtree[u] = new ArrayList<>();
+        order[in[u] = tick++] = u;
         for(int v: adj[u]) 
             if(v != p) {
                 calc_size(v, u);
                 size[v] += size[u];
                 if(bigChild[u] == -1 || size[bigChild[u]] < size[v]) bigChild[u] = v;
             }
+        out[u] = tick - 1;
     }
     public static void dfs(int u, int p, boolean keep) {
         for(int v: adj[u])
             if(v != p && v != bigChild[u])
                 dfs(v, u, false);
-        if(bigChild[u] != -1) {
-            dfs(bigChild[u], u, true);
-            subtree[u] = subtree[bigChild[u]];
-        }
-        subtree[u].add(u); 
+        if(bigChild[u] != -1) dfs(bigChild[u], u, true);
         // add contribution of u
         for(int v: adj[u])
             if(v != p && v != bigChild[u])
-                for(int w: subtree[v]) {
-                    subtree[u].add(w);
+                for(int i = in[v]; i <= out[v]; ++i) {
+                    int w = order[i];
                     // add contribution of w
                 }
         // answer queries
         if(!keep) 
-            for(int v: subtree[u])
+            for(int i = in[u]; i <= out[u]; ++i) {
+                int v = order[i];
                 // remove contribution of v
+            }
     }
 }
