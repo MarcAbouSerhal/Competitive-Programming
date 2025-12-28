048a641ac90bf58327ee285211b5c80cda141159
MarcAbouSerhal <156357566+MarcAbouSerhal@users.noreply.github.com>
2025-12-28 17:44:20 +0200

    cleaned up `FastInput`

--- FastInput.java
+++ FastInput.java
@@ -1,44 +1,35 @@
 class FastInput { 
-    private static final int bufferSize = 1 << 18;
-    private final InputStream in;
-    private final byte[] buffer = new byte[bufferSize];
+    private static final int BUFFER_SIZE = 1 << 18, INITIAL_TOKEN_SIZE = 16;
+    private InputStream in;
+    private byte[] buffer = new byte[BUFFER_SIZE];
     private int pointer = 0, bytesRead = 0;
-    public FastInput() { this.in = System.in; } 
-    public FastInput(InputStream in) { this.in = in; }
-    private final byte read() throws IOException {
+    FastInput() { this.in = System.in; } 
+    FastInput(InputStream in) { this.in = in; }
+    private byte read() throws IOException {
         if(pointer >= bytesRead) {
             pointer = 0;
             if((bytesRead = in.read(buffer)) == -1) throw new IOException("EOF character was encountered.");
         }
         return buffer[pointer++];
     }
-    public final char nextChar() throws IOException { 
+    char nextChar() throws IOException { 
         byte b;
         while ((b = read()) <= ' ');
         return (char)b;
     }
-    public final char[] next() throws IOException { 
+    char[] next() throws IOException { 
         byte b;
         while ((b = read()) <= ' ');
-        char[] token = new char[10];
+        char[] token = new char[INITIAL_TOKEN_SIZE];
         int len = 0;
         do {
-            if(len == token.length) {
-                char[] newToken = new char[len << 1];
-                for(int i = 0; i < len; ++i) newToken[i] = token[i];
-                token = newToken;
-            }
+            if(len == token.length) token = Arrays.copyOf(token, len << 1);
             token[len++] = (char)b;
         } while((b = read()) > ' ');
         if(len == token.length) return token;
-        else {
-            char[] resizedToken = new char[len];
-            for(int i = 0; i < len; ++i) resizedToken[i] = token[i];
-            return resizedToken;
-        }
+        else return Arrays.copyOf(token, len);
     } 
-    public final String nextString() throws IOException { return new String(next()); }
-    public final int nextInt() throws IOException { 
+    int nextInt() throws IOException { 
         int num = 0, sign = 1;
         byte b;
         while ((b = read()) <= ' ');
@@ -51,7 +42,7 @@ class FastInput {
         } while ((b = read()) >= '0' && b <= '9');
         return num;
     } 
-    public final long nextLong() throws IOException { 
+    long nextLong() throws IOException { 
         long num = 0;
         int sign = 1;
         byte b;
@@ -65,37 +56,19 @@ class FastInput {
         } while ((b = read()) >= '0' && b <= '9');
         return num;
     } 
-    public final double nextDouble() throws IOException { return Double.parseDouble(new String(next())); } 
-    public final char[] nextLine() throws IOException { 
+    double nextDouble() throws IOException { return Double.parseDouble(new String(next())); } 
+    char[] nextLine() throws IOException { 
         byte b = read();
-        char[] token = new char[128];
+        char[] token = new char[INITIAL_TOKEN_SIZE];
         int len = 0;
         do {
-            if(len == token.length) {
-                char[] newToken = new char[len << 1];
-                for(int i = 0; i < len; ++i) newToken[i] = token[i];
-                token = newToken;
-            }
+            if(len == token.length) token = Arrays.copyOf(token, len << 1);
             token[len++] = (char)b;
         } while((b = read()) != '\n');
         if(len == token.length) return token;
-        else {
-            char[] resizedToken = new char[len];
-            for(int i = 0; i < len; ++i) resizedToken[i] = token[i];
-            return resizedToken;
-        }
+        else return Arrays.copyOf(token, len);
     } 
-    public final int[] nextIntArray(int n) throws IOException {
-        int[] array = new int[n];
-        for(int i = 0; i < n; ++i) array[i] = nextInt();
-        return array;
-    }
-    public final long[] nextLongArray(int n) throws IOException {
-        long[] array = new long[n];
-        for(int i = 0; i < n; ++i) array[i] = nextLong();
-        return array;
-    }
-    public final void skipToken() throws IOException {
+    void skipToken() throws IOException {
         while (read() <= ' ');
         while (read() > ' ');
     }
